8549474e2b8c90f5feff004688d51f279428a6b4
h1ybz <140746668+h1ybz@users.noreply.github.com>
2024-02-10 15:26:38 -0800

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def main():
         dust_off(yaws=[0.2, 0],
                 pitches=[0.3, 0],
                 rolls=[0.4, 0],
-                                 sleep_after_point_reached=1)
+                sleep_after_point_reached=1)
 
         # Make Spot to move by goal_x meters forward and goal_y meters left
         spot.move_to_goal(goal_x=0.5, goal_y=0)
