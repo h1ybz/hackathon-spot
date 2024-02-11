796727ea80438e22b6716a1fe578d3ee92431cad
h1ybz <140746668+h1ybz@users.noreply.github.com>
2024-02-10 16:26:39 -0800

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def main():
                                  sleep_after_point_reached=1)
         time.sleep(1)
         
-        spot.stand_command(body_height=200.0, footprint_R_body="Yaw('0')-Roll('0')-Pitch('0.3')")
+        spot.stand_command(body_height=200.0, footprint_R_body=EulerZXY(yaw=0.4, roll=0.0, pitch=0.0))
         time.sleep(3)
 
         # Make Spot to move by goal_x meters forward and goal_y meters left
